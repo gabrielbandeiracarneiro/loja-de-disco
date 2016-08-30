b76d68ab170dff42ec0573a5dc2fef97f1b3efc0
gabrieljoyki <gabriel.joyki@gmail.com>
2016-08-30 14:43:12 -0300

    Update banco de dados.sql

--- banco de dados.sql
+++ banco de dados.sql
@@ -1,6 +1,17 @@
-﻿create schema estoque;
+create schema estoque;
 
 create table estoque.genero(
 	id_genero integer primary key,
 	ds_genero varchar(45)
+);
+
+create table estoque.artista(
+	id_artista integer primary key,
+	ds_artista varchar(45)
+);
+
+create table estoque.faixa_midia(
+	id_faixa integer primary key,
+	ds_faixa varchar(45),
+	tempo_faixa time
 );

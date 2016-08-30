4bc01667da3a3a448403a56de796c8fd441c5baa
gabrieljoyki <gabriel.joyki@gmail.com>
2016-08-30 14:51:33 -0300

    Update banco de dados.sql

--- banco de dados.sql
+++ banco de dados.sql
@@ -15,3 +15,22 @@ create table estoque.faixa_midia(
 	ds_faixa varchar(45),
 	tempo_faixa time
 );
+
+create table estoque.gravadora(
+	id_gravadora integer primary key,
+	ds_gravadora varchar(45)
+);
+
+create table estoque.artista_gravadora_faixa(
+	id integer primary key,
+	nu_seqmidia integer,
+	nu_seq_faixa integer,
+	nu_seq_artista integer,
+	nu_seq_gravadora integer
+);
+
+create table estoque.midia(
+	id_midia integer primary key,
+	ds_midia varchar(45),
+	nu_seq_categoria integer
+);
